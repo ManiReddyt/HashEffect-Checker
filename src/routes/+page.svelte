85ef47bf48aae0100ada179ef2d8a8eb5f32004e
Divakar <81960004+diva-21@users.noreply.github.com>
2023-07-29 13:14:00 +0530

    added routes (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,36 @@
-<h1 class="text-3xl">Hash Effect</h1>
-<div class="flex flex-col">
-    <p>SSS</p>
-    <p>VSS</p>
-    <p>FROST</p>
-</div>
+<script>
+  import {page} from '$app/stores';
+</script>
+
+<section class="">
+  <div class="w-fit mx-auto">
+    <h1 class="mx-auto text-4xl font-bold ">
+      Welcome to <span class="text-blue-500">HashEffect Test Tool</span> 
+    </h1>
+  </div>
+    <nav class="w-fit mx-auto mt-5">
+      <ul class="flex text-center space-x-5 mt-2 font-medium">
+        <li aria-current={$page.url.pathname === '/sss' ? 'page' : undefined}>
+          <a href="/sss">SSS</a>
+        </li>
+        <li aria-current={$page.url.pathname === '/vss' ? 'page' : undefined}>
+          <a href="/vss">VSS</a>
+        </li>
+        <li aria-current={$page.url.pathname === '/schnor' ? 'page' : undefined}>
+          <a href="/schnor">SCHNOR</a>
+        </li>
+      </ul>
+    </nav>
+</section>
+
+<style lang="postcss">
+  /* :global(html) {
+    background-color: theme(colors.gray.100);
+  } */
+  section {
+    /* display: flex; */
+    /* align-items: center; */
+    /* flex-direction: column; */
+    /* margin-top: 30px; */
+  }
+</style>
---
+++ src/routes/sss/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<title>SSS</title>
+	<meta name="SSS Page" content="SSS Page" />
+</svelte:head>
+
+<section>
+	<div class="w-fit mx-auto">
+		<h1 class="mx-auto text-4xl font-bold ">
+		  Welcome to <span class="text-blue-500">HashEffect Test Tool</span> 
+		</h1>
+	  </div>
+	<div class="">
+		<h1 class="font-medium text-blue-500">SSS PAYLOAD</h1>
+		<div>
+			<label for="name">Secret</label>
+			<input
+			class="border-2 border-gray-300 p-2 w-full"
+			  type="text"
+			  id="secret"
+			  name="secret"
+			  value=""
+			/>
+		</div>
+		<pre>
+
+		</pre>
+	</div>
+</section>
