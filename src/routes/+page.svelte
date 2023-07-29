fa449dd7918ff421a38faa086dddcf9b6c6600bd
Animesh-03 <anivad03@gmail.com>
2023-07-29 12:59:29 +0530

    setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl">Hash Effect</h1>
+<div class="flex flex-col">
+    <p>SSS</p>
+    <p>VSS</p>
+    <p>FROST</p>
+</div>
